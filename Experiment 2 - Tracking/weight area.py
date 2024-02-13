fe623df9290a6e7681e59e7e25eaabf5006d159a
Alecampoy <alecampoy@protonmail.com>
2024-02-13 17:37:56 +0100

    Update weight area.py

--- Experiment 2 - Tracking/weight area.py
+++ Experiment 2 - Tracking/weight area.py
@@ -5,25 +5,33 @@ Created on Tue Feb 13 12:27:09 2024
 @author: Ale Campoy
 """
 import numpy as np
-from random import randrange, uniform
 
 R = 1
+r = R * np.sqrt(
+    np.random.uniform(0, 1, 5000)
+)  # distribución homogenea sobre una circunferencia
 
-r = R * np.sqrt(np.random.uniform(0, 1, 1000))
-weights = np.arange(0, 1 + 1 / 10, 1 / 10)
+nbins = 10
+bins = np.arange(0, 1 + (1 / (nbins)), 1 / (nbins))
+weights_a = np.pi * np.arange(0, 1 + (1 / (nbins)), 1 / (nbins)) ** 2
+weights_a = np.diff(weights_a)  # [:len(weights)-1]
 
-bins = np.arange(0, 1 + 1 / 12, 1 / 12)
-weights = np.pi * (bins**2)
+bin_of_r = np.searchsorted(bins, r) - 1  #
+weights_a_ind = 1 / weights_a[bin_of_r]
+# b = np.diff(np.searchsorted(bins, r))
+weights_r = 1 / (np.pi * r)  # dada la naturaleza radial de los datos
 
-weights = np.diff(weights)  # [:len(weights)-1]
-
-weights = np.repeat(weights, np.diff(np.searchsorted(bins, r)))
-a = np.diff(np.searchsorted(bins, r))
-# weights = 1/(r)
+# %%
 import matplotlib.pyplot as plt
 
-count, bins, ignored = plt.hist(r, 12, weights=weights, density=True)
+
+count, bins = np.histogram(r, bins, density=True)
+plt.hist(r, bins, weights=weights_r, density=True, alpha=0.5, color="b")
+plt.hist(r, bins, weights=weights_a_ind, density=True, alpha=0.5, color="g")
 
 plt.plot(bins, np.ones_like(bins), linewidth=2, color="r")
+plt.plot(
+    bins[: len(bins) - 1] + 0.05, count / (np.pi * weights_a), linewidth=2, color="g"
+)
 
 plt.show()
